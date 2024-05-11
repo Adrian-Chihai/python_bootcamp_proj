9d0a56c216fb3c06cbe59b7024bd7895c39f635b
Adrian Chihai <cadrian1602@yahoo.com>
2024-05-11 20:39:42 +0300

    first commit

---
+++ playlist_mover/playlist_collector.py
@@ -0,0 +1,26 @@
+import spotipy
+import os
+
+from spotipy.oauth2 import SpotifyClientCredentials
+
+class PlaylistCollector:
+    def __init__(self):
+        self.__client_credentials_manager = SpotifyClientCredentials(os.getenv("SPOTIFY_CLIENT_ID"), os.getenv("SPOTIFY_CLIENT_SECRET"))
+        self.__sp = spotipy.Spotify(client_credentials_manager=self.__client_credentials_manager)
+
+    def get_playlist(self):
+        playlist_url = input("Enter a playlist URL (playlist needs to be public): ")
+        self.__get_playlist_title(playlist_url)
+        self.__get_playlist_songs(playlist_url)
+
+    def __get_playlist_title(self, playlist_url):
+        return self.__sp.playlist(playlist_url)['name']
+
+    def __get_playlist_songs(self, playlist_url):
+        playlist_data = self.__sp.playlist_items(playlist_url)
+        for song in playlist_data['items']:
+            track = song['track']
+            artist = track['artists'][0]['name']
+            song_name = track['name']
+            album_name = track['album']['name']
+            print(f"Song: {artist}:{song_name} - Album: {album_name}")
---
+++ playlist_mover/test.py
@@ -0,0 +1,5 @@
+from playlist_collector import PlaylistCollector
+
+playlist_collector = PlaylistCollector()
+
+playlist_collector.get_playlist()
